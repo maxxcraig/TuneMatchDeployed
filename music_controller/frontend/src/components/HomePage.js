85fbd9d29ee58fb985fbc621b98afcc508e43da1
MaxCraig <max8alton@gmail.com>
2025-04-21 15:05:35 -0700

    UI changes and component updates

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,25 +2,32 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from './Room';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
-
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 export default class HomePage extends Component {
     constructor(props){
-        super(props)
+        super(props);
     }
+
     render(){
         return (
             <Router>
                 <Switch>
-                    <Route exact path="/"><p>This is the home page</p></Route>
+                    <Route exact path="/">
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            height: "100vh",
+                            flexDirection: "column"
+                        }}>
+                            <p>This is the home page</p>
+                        </div>
+                    </Route>
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
-                    <Route path='/room/:roomCode' component={Room} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
-
-
-
             </Router>
         );
     }
