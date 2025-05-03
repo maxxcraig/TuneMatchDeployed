9726257cae5efa82fe0c8a2224549c07cd7fd0dd
MaxCraig <max8alton@gmail.com>
2025-05-02 17:00:34 -0700

    Move Spotify credentials to environment variables

--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -1,3 +1,5 @@
-CLIENT_ID = "a0e199764dc24b0dbd39188336e3da09"
-CLIENT_SECRET = "a9844f6fd5304a4f92e8ffa18ca02a78"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+import os
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+REDIRECT_URI = os.environ.get("REDIRECT_URI")
